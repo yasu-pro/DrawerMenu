d0a33ca9848baa98b8ac5a281be61bf333406c7c
yasu-pro <cb1-y.s@docomo.ne.jp>
2020-11-02 21:43:20 +0900

    fixed style.css, index.js, index.html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const button = document.getElementById("drawer__button");
+const button = document.getElementById("js-drawer");
 const drawer = document.getElementById("drawer__menu");
 
 let isSlide = true;
@@ -21,9 +21,9 @@ drawer.addEventListener("resize", () => {
 });
 
 button.addEventListener("click", () => {
-  let clientReact = drawer.getBoundingClientRect();
-  let x = clientReact.left;
-  if (isSlide === true) {
+  const clientReact = drawer.getBoundingClientRect();
+  const x = clientReact.left;
+  if (isSlide) {
     drawer.style.left = x + client_w + "px";
     isSlide = false;
     console.log(client_w + "px");
